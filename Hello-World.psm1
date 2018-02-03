2af2b5bee32346f271612214761f47ac00f5f526
simonstdenis <simonstdenis@msn.com>
2018-02-02 21:33:28 -0500

    test
    test

---
+++ Hello-World.psm1
@@ -0,0 +1,20 @@
+﻿<#	
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2017 v5.4.136
+	 Created on:   	2/2/2018 9:25 PM
+	 Created by:   	simon
+	 Organization: 	
+	 Filename:     	Hello-World.psm1
+	-------------------------------------------------------------------------
+	 Module Name: Hello-World
+	===========================================================================
+#>
+
+function Say-Hello
+{
+	Write-Output "Hello Simon!"
+	
+}
+
+
+Export-ModuleMember -Function Say-Hello
